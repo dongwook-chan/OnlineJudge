6d1f2817298274812c66f0ff3706b287bb1fb819
dongwook-chan <dongwook.chan@gmail.com>
2020-12-26 03:11:42 +0900

    solved problem 1110

---
+++ BOJ/1110/1110.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int N, M;
+    cin >> N;
+    M = N;
+
+    int i = 0;
+    do {
+        int adj_M = (M / 10) ? M : M * 10;
+        M = (M % 10) * 10 + (adj_M / 10 + adj_M % 10) % 10;
+        //cout << M << endl;;
+        ++i;
+        if(i == 70) break;
+    } while (M != N);
+
+    cout << i;
+    return 0;
+}
