f83d896b31a70eb0b489fd79acd26ac872ce01b8
dongwook-chan <dongwook.chan@gmail.com>
2020-12-25 20:58:42 +0900

    solved problem 2739

--- BOJ/2739/2739.cpp
+++ BOJ/2739/2739.cpp
@@ -1,13 +1,14 @@
-#include <stdio.h>
+#include <iostream>
+#include <format>
 
 using namespace std;
 
 int main(){
     int N;
-    scanf("%d", &N);
+    cin >> N;
 
     for (int i = 1; i <= 9; ++i) {
-        printf("%d * %d = %d\n", N, i, N * i);
+        cout << format("{} * {} = {}", N, i, N * i);
     }
 
     return 0;
