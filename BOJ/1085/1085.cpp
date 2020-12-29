fee2c0aed73773be8202ffb926e201ae9ae39ac3
dongwook-chan <dongwook.chan@gmail.com>
2020-12-29 23:13:03 +0900

    solved problem 1085

---
+++ BOJ/1085/1085.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int x, y, w, h;
+    cin >> x >> y >> w >> h;
+
+    cout << min({h - y, y, w - x, w});
+
+    return 0;
+}
