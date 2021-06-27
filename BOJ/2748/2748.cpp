e737114a9d2ace2b75f8d5c5eeb574266fe09c6d
dongwook-chan <dongwook.chan@gmail.com>
2021-06-27 20:35:56 +0900

    solved problem 2748

---
+++ BOJ/2748/2748.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+#define NIL -1
+
+using namespace std;
+
+vector<long long> lookup (46, NIL);
+
+int fib (int n){
+    if (lookup[n] == NIL){
+        if (n <= 1){
+            lookup[n] = n;
+        }
+        else{
+            lookup[n] = fib(n - 1) + fib(n - 2);
+        }
+    }
+    return lookup[n];
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n;
+    cin >> n;
+
+    cout << fib(n);
+
+    return 0;
+}
