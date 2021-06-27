83929696649d55a6a155c3dc51e1ef630880adc0
dongwook-chan <dongwook.chan@gmail.com>
2021-06-27 22:27:43 +0900

    solved problem 15624

---
+++ BOJ/15624/15624.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+
+#define NIL -1
+
+using namespace std;
+
+int fib (int n, vector<int>& lookup){
+    if (lookup[n] == NIL){
+        if (n <= 1){
+            lookup[n] = n;
+        }
+        else{
+            lookup[n] = (fib(n - 1, lookup) + fib(n - 2, lookup)) % 1000000007;
+        }
+    }
+    return lookup[n];
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n;
+    cin >> n;
+
+    vector<int> lookup (1000000, NIL);
+    cout << fib(n, lookup);
+
+    return 0;
+}
