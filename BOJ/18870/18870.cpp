141d7c7f5b589c2e0742d819bd6defb8375d031b
dongwook-chan <dongwook.chan@gmail.com>
2021-11-11 02:35:51 +0900

    solved problem 18870

--- BOJ/18870/18870.cpp
+++ BOJ/18870/18870.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
-#include <set>
-#include <iterator>
+#include <unordered_map>
+#include <vector>
+#include <numeric>
+#include <algorithm>
 
 using namespace std;
 
@@ -12,17 +14,50 @@ int main(){
     int N;
     cin >> N;
 
-    int Xs[1000000];
-    set<int> Xs_sorted;
-    for(int i = 0; i < N; ++i) {
+    int *X_of_idx = new int[1000000];
+    unordered_map<int, int> idx_of_X;
+    for(int idx = 0; idx < N; ++idx) {
         int X;
         cin >> X;
-        Xs[i] = X;
-        Xs_sorted.insert(X);
+        X_of_idx[idx] = X;
+        idx_of_X[X] = idx;
     }
 
-    for (int i = 0; i < N; ++i) {
-        cout << distance(Xs_sorted.begin(), Xs_sorted.find(Xs[i])) << ' ';
+    int *idx_of_rank = new int[1000000];
+    int i = 0;
+    for (auto it = idx_of_X.begin(); it != idx_of_X.end(); ++it) {
+        idx_of_rank[i++] = it->second;
+    }
+
+    int M = idx_of_X.size();
+
+    /*
+    for (int i = 0; i < M; ++i) {
+        cout << idx_of_rank[i] << " ";
+    }
+    cout << endl;
+    */
+    
+    sort(idx_of_rank, idx_of_rank + M, [&X_of_idx](int const &idx1, int const &idx2) -> bool const{
+        return X_of_idx[idx1] < X_of_idx[idx2];
+    });
+
+    /*
+    for (int i = 0; i < M; ++i) {
+        cout << idx_of_rank[i] << " ";
+    }
+    cout << endl;
+    */
+
+    int *rank_of_idx = new int[1000000];
+    for (int rank = 0; rank < M; ++rank) {
+        int idx = idx_of_rank[rank];
+        rank_of_idx[idx] = rank;
+    }
+
+    //cout << "final" << endl;
+    for (int idx = 0; idx < N; ++idx) {
+        cout << rank_of_idx[idx_of_X[X_of_idx[idx]]] << " ";
     }
 
     return 0;
