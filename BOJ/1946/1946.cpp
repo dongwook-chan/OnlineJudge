2bd0213fa21381ac13a7c2383d6112c40b183678
dongwook-chan <dongwook.chan@gmail.com>
2021-11-11 00:50:12 +0900

    solved problem 1946

--- BOJ/1946/1946.cpp
+++ BOJ/1946/1946.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
-#include <numeric>
-#include <cstring>
+#include <vector>
 #include <algorithm>
+#include <list>
 
 using namespace std;
 
@@ -16,18 +16,38 @@ int main(){
         int N;
         cin >> N;
 
-        int rank_of_idx[100000][2];
+        vector<pair<int, int>> rank_of_idx (N, pair<int, int>());
         for (int i = 0; i < N; ++i) {
-            cin >> rank_of_idx[i][0];
-            cin >> rank_of_idx[i][1];
-            --rank_of_idx[i][0];
-            --rank_of_idx[i][1];
+            cin >> rank_of_idx[i].first;
+            cin >> rank_of_idx[i].second;
+            --rank_of_idx[i].first;
+            --rank_of_idx[i].second;
         }
 
-        sort(rank_of_idx, rank_of_idx + N, [](int const *rank1, int const *rank2) -> bool const {
-            return rank1[0] < rank2[0];
+        sort(rank_of_idx.begin(), rank_of_idx.end(), [](pair<int, int> const &rank1, pair<int, int> const &rank2) -> bool const {
+            return rank1.first < rank2.first;
         });
 
+        //cout << "sorted" << endl;
+
+        list<pair<int, int>> rank (rank_of_idx.begin(), rank_of_idx.end());
+
+        //cout << "listed" << endl;
+
+        for (list<pair<int, int>>::iterator it1 = rank.begin(); it1 != rank.end(); ++it1) {
+            list<pair<int, int>>::iterator it2 = it1;
+            for (++it2; it2 != rank.end();) {
+                //cout << it1->first << "-" << it1->second << ", " << it2->first << "-" << it2->second << endl;  
+                if (it2->second > it1->second) {
+                    it2 = rank.erase(it2);
+                }
+                else {
+                    ++it2;
+                }
+            }
+        }
+
+        cout << rank.size() << '\n';
     }
 
     return 0;
