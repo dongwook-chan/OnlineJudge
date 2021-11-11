867257dbc1730ce813c2ad8b79ad0b080e112b0c
dongwook-chan <dongwook.chan@gmail.com>
2021-11-12 02:14:57 +0900

    solved problem 1107

---
+++ BOJ/1107/1107.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int N, M;
+bool err_dgt[10];
+vector<int> dgts;
+int chl_len;
+int answer;
+
+void bf(int pos, int chl){
+    if (pos == chl_len) {
+        answer = min(answer, chl_len + abs(N - chl));
+        return;
+    }
+
+    for (int dgt : dgts) {
+        bf(pos + 1, chl * 10 + dgt);
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    /*
+        greedily select digit buttons first then up/down buttons
+            case 1) 100 (no digit selection) -> up/down buttons
+            case 2) digit selections -> up/down buttons
+    */
+
+    cin >> N >> M;
+    for (int i = 0; i < M; ++i) {
+        int dgt;
+        cin >> dgt;
+        err_dgt[dgt] = true;
+    }
+
+    for (int dgt = 0; dgt < 10; ++dgt) {
+        if (err_dgt[dgt]) continue;
+        dgts.push_back(dgt);
+    }
+
+    answer = abs(N - 100);
+
+    chl_len = to_string(N).size();
+    bf(0, 0);
+
+    cout << answer;
+
+    return 0;
+}
