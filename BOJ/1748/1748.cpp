9d5fbd98c789a203f2fef61d4ae9d5ddd3e5e35c
dongwook-chan <dongwook.chan@gmail.com>
2021-11-12 18:43:49 +0900

    solved problem 1748

--- BOJ/1748/1748.cpp
+++ BOJ/1748/1748.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 int N;
 long long ans;
+int no_bounds[] = { 1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000 };
 
 int main(){
     ios_base::sync_with_stdio(0);
@@ -13,8 +14,12 @@ int main(){
 
     cin >> N;
 
-    for (int no = 1; no <= N; ++no) {
-        ans += to_string(no).size();
+    int no;
+    for (int i = 0; i < 9; ++i) {
+        int no_bound = no_bounds[i + 1];
+        for (no = no_bounds[i] ; no < no_bound && no <= N; ++no) {
+            ans += i + 1;
+        }
     }
 
     cout << ans;
