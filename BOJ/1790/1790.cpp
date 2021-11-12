02731e3a55964d9a9fe0ce92b0a76020ef456f12
dongwook-chan <dongwook.chan@gmail.com>
2021-11-12 22:22:24 +0900

    solved problem 1790

--- BOJ/1790/1790.cpp
+++ BOJ/1790/1790.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 int N, K;
+int no_bounds[] = { 1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000 };
 
 int main(){
     ios_base::sync_with_stdio(0);
@@ -12,15 +13,23 @@ int main(){
 
     cin >> N >> K;
 
-    for (int no = 1; no <= N; ++no) {
-        string no_str = to_string(no);
-        for (char c : no_str) {
-            if(--K == 0) {
-                cout << c;
+    int no = 1;
+    for (int i = 1; i < 10; ++i) {
+        for (; no < no_bounds[i] && no <= N; ++no) {
+            //cout << K << ", " << no << endl;
+            if (K - i <= 0) {
+                string no_str = to_string(no);
+                cout << no_str[K - 1];
+
                 return 0;
+            }
+            else {
+                K -= i;
             }
         }
     }
+
     cout << -1;
+
     return 0;
 }
