40858482b2294149649be5cebf9a7440322f6b90
dongwook-chan <dongwook.chan@gmail.com>
2021-11-13 18:10:56 +0900

    solved problem 3085

--- BOJ/3085/3085.cpp
+++ BOJ/3085/3085.cpp
@@ -8,32 +8,32 @@ int N;
 string brd[50];
 int ans;
 
-void eat(){
+void eat_row(int y){
 	int clr_ctr;
 	
-	for (int y = 0; y < N; ++y) {
-		clr_ctr = 1;
-		for (int x = 1; x < N; ++x) {
-			if (brd[y][x - 1] == brd[y][x]) {
-				++clr_ctr;
-				ans = max(ans, clr_ctr);
-			}
-			else {
-				clr_ctr = 1;
-			}
+	clr_ctr = 1;
+	for (int x = 1; x < N; ++x) {
+		if (brd[y][x - 1] == brd[y][x]) {
+			++clr_ctr;
+			ans = max(ans, clr_ctr);
+		}
+		else {
+			clr_ctr = 1;
 		}
 	}
+}
 
-	for (int x = 0; x < N; ++x) {
-		clr_ctr = 1;
-		for (int y = 1; y < N; ++y) {
-			if (brd[y - 1][x] == brd[y][x]) {
-				++clr_ctr;
-				ans = max(ans, clr_ctr);
-			}
-			else {
-				clr_ctr = 1;
-			}
+void eat_col(int x){
+	int clr_ctr;
+
+	clr_ctr = 1;
+	for (int y = 1; y < N; ++y) {
+		if (brd[y - 1][x] == brd[y][x]) {
+			++clr_ctr;
+			ans = max(ans, clr_ctr);
+		}
+		else {
+			clr_ctr = 1;
 		}
 	}
 }
@@ -52,7 +52,9 @@ int main(){
 	for (int y = 0; y < N; ++y) {
 		for (int x = 0; x < N - 1; ++x) {
 			swap(brd[y][x], brd[y][x + 1]);
-			eat();
+			eat_row(x);
+			eat_row(x + 1);
+			eat_col(y);
 			swap(brd[y][x], brd[y][x + 1]);
 		}
 	}
@@ -61,7 +63,9 @@ int main(){
 	for (int y = 0; y < N - 1; ++y) {
 		for (int x = 0; x < N; ++x) {
 			swap(brd[y][x], brd[y + 1][x]);
-			eat();
+			eat_row(x);
+			eat_col(y);
+			eat_col(y + 1);
 			swap(brd[y][x], brd[y + 1][x]);
 		}
 	}
