f37352e6720674f1a984f776a4eb8c787bfdbb7f
dongwook-chan <dongwook.chan@gmail.com>
2021-11-29 22:59:37 +0900

    solved problem 4153

--- BOJ/4153/4153.cpp
+++ BOJ/4153/4153.cpp
@@ -8,7 +8,6 @@ int sides[3];
 
 int main () {
     while (true) {
-        int a, b, c;
         cin >> sides[0] >> sides[1] >> sides[2];
 
         if (sides[0] == sides[1] && sides[1] == sides[2] && sides[2] == 0) {
@@ -23,12 +22,11 @@ int main () {
         
         string ans;
         if (sides[2] == sides[1] + sides[0]) {
-            ans = "right";
+            cout << "right" << endl;
         }
         else {
-            ans = "wrong";
+            cout << "wrong" << endl;
         }
-        cout << ans << '\n';
 
     }
 
