2f68dd5db1101e3d32446c586098c6db779e2f5d
dongwook-chan <dongwook.chan@gmail.com>
2022-03-15 01:49:26 +0900

    solved problem 2443

--- BOJ/2443/2443.cpp
+++ BOJ/2443/2443.cpp
@@ -8,7 +8,7 @@ int main() {
     cin >> N;
 
     for (int i = 0; i < N; ++i) {
-        cout << string(' ', i) << string('*', 2 * (N - i) - 1) << '\n';
+        cout << string(i, ' ') << string(2 * (N - i) - 1, '*') << '\n';
     }
 
     return 0;
