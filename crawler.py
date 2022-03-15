9be4758c9d90044006d92f7793b544960b180eac
dongwook-chan <dongwook.chan@gmail.com>
2022-03-15 22:49:47 +0900

    Add header to GET request

--- crawler.py
+++ crawler.py
@@ -4,7 +4,6 @@ from bs4 import BeautifulSoup
 import sys
 import os
 
-print(sys.argv)
 url = "https://www.acmicpc.net/problem/" + sys.argv[2]
 
 '''
@@ -20,8 +19,16 @@ proxies = {
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 req = requests.get(url, proxies=proxies, verify=False)
 '''
-
-req = requests.get(url)
+headers = {
+    'accept': 'text/html',
+    'accept-language': 'ko-KR,ko;',
+    'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="99", "Google Chrome";v="99"',
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36'
+}
+req = requests.get(url, headers=headers)
+if req.status_code != 200:
+    print(f"GET request returned status code {req.status_code}")
+    sys.exit(1)
 soup = BeautifulSoup(req.text,"html.parser")
 tags = soup.findAll('pre', attrs={'class': 'sampledata'})
 
@@ -33,3 +40,4 @@ for i in range(len(tags)):
     with open(os.path.join(sys.argv[1], file_name), 'w') as fp:
         print(os.path.join(sys.argv[1], file_name))
         fp.write(tags[i].get_text())
+
