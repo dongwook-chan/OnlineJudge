d1b27a2f1746849539fcdb15f865e9766d172f62
dongwook-chan <dongwook.chan@gmail.com>
2022-03-30 00:26:49 +0900

    solved problem 1463

--- BOJ/1463/1463.cpp
+++ BOJ/1463/1463.cpp
@@ -1,3 +1,46 @@
+// Memoization(Top-Down)
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+
+#define MAX_NO 1000000
+
+using namespace std;
+
+unsigned int memo[MAX_NO + 1];
+
+unsigned int lookup(unsigned int N) {
+    if(N == 1){
+        return 0;
+    }
+    if(memo[N] != -1){
+        return memo[N];
+    }
+
+    if(N % 3 == 0){
+        memo[N] = min(memo[N], lookup(N / 3) + 1);
+    }
+    if(N % 2 == 0){
+        memo[N] = min(memo[N], lookup(N / 2) + 1);
+    }
+    memo[N] = min(memo[N], lookup(N - 1) + 1);
+
+    return memo[N];
+}
+
+int main(){
+    memset(memo, -1, sizeof(memo));
+
+    unsigned int N;
+    cin >> N;
+
+    cout << lookup(N);
+
+    return 0;
+}
+
+/*
+// Tabulation(Bottom-Up)
 #include <cstring>
 #include <iostream>
 #include <algorithm>
@@ -29,3 +72,4 @@ int main() {
     cout << memo[N];
     return 0;
 }
+*/
