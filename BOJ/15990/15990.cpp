deb63bdb024aae85634c3b0ced35ff6b9c43eb05
dongwook-chan <dongwook.chan@gmail.com>
2022-04-03 02:04:11 +0900

    solved problem 15990

---
+++ BOJ/15990/15990.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <cstring>
+
+#define MAX_NO 100000
+
+using namespace std;
+
+int table_[MAX_NO][3] = {
+    {1, 0, 0}, {0, 1, 0}, {1, 1, 1}
+};
+int *table[MAX_NO + 1];
+
+// table[1][1] == table_[0][0]
+void create_table() {
+    for(int i = 1; i <= MAX_NO; ++i) {
+        table[i] = table_[i - 1] - 1;
+    }
+}
+
+int lookup(int n, int m) {
+    switch(n){
+        case 1: case 2: case 3:
+            return table[n][m];
+    }
+
+    if(table[n][m]) return table[n][m];
+
+    for(int i = 1; i <= 3; ++i) {
+        if (i == m) continue;
+        table[n][m] += lookup(n - m, i);
+    }
+    return table[n][m];
+}
+
+int main() {
+    create_table();
+
+    lookup(4,1);
+
+    int T;
+    cin >> T;
+    while(T--) {
+        int n;
+        cin >> n;
+
+        cout << lookup(n,1) + lookup(n,2) + lookup(n,3) << '\n';
+    }
+    return 0;
+}
