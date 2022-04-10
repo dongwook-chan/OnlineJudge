a04274853f9d61a0b6158527ea56577cef07cd38
dongwook-chan <dongwook.chan@gmail.com>
2022-04-11 02:32:22 +0900

    solved problem 23290

--- BOJ/23290/23290.cpp
+++ BOJ/23290/23290.cpp
@@ -33,80 +33,19 @@ int dr[] = {0, 0, -1, -1, -1, 0, 1, 1, 1};
 int dc[] = {0, -1, -1, 0, 1, 1, 1, 0, -1};
 bool fish_smell[MAX_GRID + 1][MAX_GRID + 1][MAX_REP + 2];
 bool check_fish_smell(int r, int c, int s) {
-    for(int ns = s; ns < MAX_REP && ns <= s + 2; ++ns) {
+    for(int ns = s; ns < MAX_REP && ns < s + 2; ++ns) {
         if(fish_smell[r][c][ns]) return true;
     }
     return false;
 }
-void print_fish_smell(int s) {
-#ifdef DEBUG
-    for(int r = 1; r <= 4; ++r) {
-        for(int c = 1; c <= 4; ++c) {
-            bool smelly = false;
-            for(int ns = s; ns < MAX_REP && ns <= s + 2; ++ns) {
-                if(fish_smell[r][c][ns]) {
-                    smelly = true;
-                    cout << ns;
-                }
-            }
-            if(!smelly) cout << '-';
-            cout << '\t';
-        }
-        cout << endl;
-    }
-    cout << endl;
-#endif
-    ;
-}
 vector<int> fish_traveled[MAX_GRID + 1][MAX_GRID + 1];
-void print_fish(vector<int> fish[MAX_GRID + 1][MAX_GRID + 1]) {
-#ifdef DEBUG
-    for(int r = 1; r <= 4; ++r) {
-        for(int c = 1; c <= 4; ++c) {
-            if(fish[r][c].empty()) {
-                cout << '-' << '\t';
-                continue;
-            }
-            for(int f : fish[r][c]) {
-                cout << f;
-            }
-            cout << '\t';
-        }
-        cout << endl;
-    }
-    cout << endl;
-#endif
-    ;
-}
-void print_grid(int grid[MAX_GRID + 1][MAX_GRID + 1]) {
-#ifdef DEBUG
-    for(int r = 1; r <= 4; ++r) {
-        for(int c = 1; c <= 4; ++c) {
-            cout << grid[r][c] << '\t';
-        }
-        cout << endl;
-    }
-    cout << endl;
-#endif
-    ;
-}
 
 // #3
-//bool visited[MAX_GRID + 1][MAX_GRID + 1];
 int dir_trace[3];
 int max_fish_ctr;
 int min_dir_trace[3];
 // 상은 1, 좌는 2, 하는 3, 우는 4로 변환한다.
 vector<int> prioritized_dir = {UP, LEFT, DOWN, RIGHT};
-int dir_to_int[] = {0, 2, 0, 1, 0, 4, 0, 3, 0};
-int convert_dir_to_int(int dir[3]) {
-    int int_ = 0;
-    for(int i = 0, m = 100; i < 3; ++i, m *= 10) {
-        int_ += dir_to_int[dir[i]] * m;
-    }
-    return int_;
-}
-vector<int> fish_to_eat[MAX_GRID + 1][MAX_GRID + 1];
 void shark_travel(int r, int c, int move_ctr, int fish_ctr) {
     //visited[r][c] = true; // NOTE: 지나온 칸도 재방문 가능
     vector<int> fish_tmp;
@@ -117,12 +56,11 @@ void shark_travel(int r, int c, int move_ctr, int fish_ctr) {
     }
 
     if(move_ctr >= 3) {
-        //cout << dir_trace << endl;
         if(fish_ctr > max_fish_ctr) {    // 가능한 이동 방법 중에서 제외되는 물고기의 수가 가장 많은 방법으로 이동하며, 그러한 방법이 여러가지인 경우 사전 순으로 가장 앞서는 방법을 이용한다.
             max_fish_ctr = fish_ctr;
             memcpy(min_dir_trace, dir_trace, sizeof(dir_trace));
         }
-        goto reset_visited;   // NOTE: return해버리면 visited reset이 안 됨...
+        goto undo_visit;   // NOTE: return해버리면 undo visit이 안 됨...
     } 
 
     for(int dir : prioritized_dir) {    // 상어는 현재 칸에서 상하좌우로 인접한 칸으로 이동할 수 있다. 
@@ -130,18 +68,16 @@ void shark_travel(int r, int c, int move_ctr, int fish_ctr) {
         int nc = c + dc[dir];
 
         if(OOG(nr, nc)) continue;   // 연속해서 이동하는 칸 중에 격자의 범위를 벗어나는 칸이 있으면, 그 방법은 불가능한 이동 방법이다.
-        //if(visited[nr][nc]) continue;
 
         dir_trace[move_ctr] = dir;
 
         shark_travel(nr, nc, move_ctr + 1, fish_ctr);
     }
 
-reset_visited:
+undo_visit:
     if(move_ctr) {
         fish[r][c] = fish_tmp;
     }
-    //visited[r][c] = false;
 }
 
 int main() {
@@ -162,8 +98,6 @@ int main() {
 #endif
         // #1 상어가 모든 물고기에게 복제 마법을 시전한다. 복제 마법은 시간이 조금 걸리기 때문에, 아래 5번에서 물고기가 복제되어 칸에 나타난다.
         copy_grid(fish_replicated, fish);
-
-        print_fish(fish);
 
         // #2 모든 물고기가 한 칸 이동한다.
         for(int r = 1; r <= 4; ++r) {
@@ -197,7 +131,6 @@ int main() {
             }
         }
         copy_grid(fish, fish_traveled);
-        print_fish(fish);
 
         // #3 상어가 연속해서 3칸 이동한다.
         for(int r = 1; r <= 4; ++r) {
@@ -208,19 +141,12 @@ int main() {
 
                 //memset(visited, false, sizeof(visited));
                 max_fish_ctr = -1;
-                copy_grid(fish_to_eat, fish);
                 shark_travel(r, c, 0, 0);
-
-            #ifdef DEBUG
-                cout << max_fish_ctr << endl;
-            #endif
 
                 int nr = r, nc = c;
                 for(int i = 0; i < 3; ++i) {
                     int dir = min_dir_trace[i];
-                #ifdef DEBUG
-                    cout << dir << " -> ";
-                #endif
+
                     nr += dr[dir];
                     nc += dc[dir];
 
@@ -229,20 +155,13 @@ int main() {
                         fish_smell[nr][nc][s + 2] = true; // 제외되는 모든 물고기는 물고기 냄새를 남긴다. 
                     }
                 }
-            #ifdef DEBUG
-                cout << endl;
-            #endif
                 shark[r][c] = false;
                 shark[nr][nc] = true;
-            #ifdef DEBUG
-                cout << nr << ", " << nc << endl;
-            #endif
                 goto end_shark_travel;
             }
         }
     end_shark_travel:
         ;
-        print_fish(fish);
 
         // #4 두 번 전 연습에서 생긴 물고기의 냄새가 격자에서 사라진다.
         for(int r = 1; r <= 4; ++r) {
@@ -250,7 +169,6 @@ int main() {
                 fish_smell[r][c][s] = false;
             }
         }
-        print_fish_smell(s);
 
         // 5. 1에서 사용한 복제 마법이 완료된다. 모든 복제된 물고기는 1에서의 위치와 방향을 그대로 갖게 된다.
         for(int r = 1; r <= 4; ++r) {
@@ -259,7 +177,6 @@ int main() {
                 fish[r][c].insert(fish[r][c].end(), fish_replicated[r][c].begin(), fish_replicated[r][c].end());
             }
         }
-        print_fish(fish);
     }
 
     int fish_ctr = 0;
