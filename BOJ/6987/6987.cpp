8fd79806a27ea0c537e450de11faf580e4b83dfd
dongwook-chan <dongwook.chan@gmail.com>
2022-04-13 02:43:43 +0900

    solved problem 6987

--- BOJ/6987/6987.cpp
+++ BOJ/6987/6987.cpp
@@ -24,15 +24,12 @@ vector<TEAM> team_to_compare_for_match[MAX_MATCH_NO + 1] = {
 };
 
 int ctr;
-void bf(int match_no, vector<int> same_as_current_score_ctr, int diff_ctr, vector<bool> diff_from_current_score) {  // NOTE: 전역 변수 undo가 어려운 경우 copy 후 자식에게 넘겨주기 
+void bf(int match_no, int diff_ctr, vector<bool> diff_from_current_score) {  // NOTE: 전역 변수 undo가 어려운 경우 copy 후 자식에게 넘겨주기 
     if(!team_to_compare_for_match[match_no].empty()) {
         for(TEAM team : team_to_compare_for_match[match_no]) {
             for(int case_ = 0; case_ < 4; ++case_) {
                 if(diff_from_current_score[case_]) continue;
-                if(!memcmp(current_score[team], input_score[case_][team], sizeof(int) * 3)) {
-                    ++same_as_current_score_ctr[case_];
-                }
-                else {
+                if(memcmp(current_score[team], input_score[case_][team], sizeof(int) * 3)) {
                     diff_from_current_score[case_] = true;
                     if(++diff_ctr == 4) return;
                 }
@@ -41,7 +38,7 @@ void bf(int match_no, vector<int> same_as_current_score_ctr, int diff_ctr, vecto
     }
     if(match_no == 15) {
         for(int case_ = 0; case_ < 4; ++case_) {
-            if(same_as_current_score_ctr[case_] == 6) {
+            if(!memcmp(current_score, input_score[case_], sizeof(int) * 18)) {
                 case_possible[case_] = true;
             }
         }
@@ -52,21 +49,21 @@ void bf(int match_no, vector<int> same_as_current_score_ctr, int diff_ctr, vecto
     // first team wins
     ++current_score[team.first][WIN];
     ++current_score[team.second][LOSE];
-    bf(match_no + 1, same_as_current_score_ctr, diff_ctr, diff_from_current_score);
+    bf(match_no + 1, diff_ctr, diff_from_current_score);
     --current_score[team.first][WIN];
     --current_score[team.second][LOSE];
 
     // tie
     ++current_score[team.first][TIE];
     ++current_score[team.second][TIE];
-    bf(match_no + 1, same_as_current_score_ctr, diff_ctr, diff_from_current_score);
+    bf(match_no + 1, diff_ctr, diff_from_current_score);
     --current_score[team.first][TIE];
     --current_score[team.second][TIE];
 
     // second team wins
     ++current_score[team.first][LOSE];
     ++current_score[team.second][WIN];
-    bf(match_no + 1, same_as_current_score_ctr, diff_ctr, diff_from_current_score);
+    bf(match_no + 1, diff_ctr, diff_from_current_score);
     --current_score[team.first][LOSE];
     --current_score[team.second][WIN];
 }
@@ -84,7 +81,7 @@ int main() {
         }
     }
    
-    bf(0, vector<int>(6, 0), 0, vector<bool>(6, false));
+    bf(0, 0, vector<bool>(6, false));
 
     for(int case_ = 0; case_ < 4; ++case_) {
         cout << (int)case_possible[case_] << ' ';
