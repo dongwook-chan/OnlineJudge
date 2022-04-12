a8dc1bff3e8fc758f96932f176c6781b5c2c23fe
dongwook-chan <dongwook.chan@gmail.com>
2022-04-12 20:29:22 +0900

    solved problem 1863

--- BOJ/1863/1863.cpp
+++ BOJ/1863/1863.cpp
@@ -1,28 +1,35 @@
 #include <iostream>
+#include <functional>
 #include <set>
+
+#define MAX_SEQUENCE_SIZE 50000
 
 using namespace std;
 
 int n;
+int y_sequence[MAX_SEQUENCE_SIZE + 1];
 int prev_y;
-set<int> ys;
+set<int, greater<int>> y_subsequence;
 int bldg_ctr;
 
 int main() {
     cin >> n;
-    while(n--) {
-        int x, y;
-        cin >> x >> y;
+    for(int i = 0; i < n; ++i) {
+        int x;
+        cin >> x >> y_sequence[i];
+    }
+    y_sequence[n] = 0;
 
-        ys.insert(y);
+    for(int i = 0; i < n; ++i) {
+        int y = y_sequence[i];
+        
+        y_subsequence.insert(y);
 
         if(y < prev_y) {
-            for(;ys.size() && *ys.begin() > y; ys.erase(ys.begin())) {
-                cout << n << ": " << x <<", " <<y << endl;
+            for(;y_subsequence.size() && *y_subsequence.begin() > y; y_subsequence.erase(y_subsequence.begin())) {
                 ++bldg_ctr;
             }
         }
-
         prev_y = y;
     }
     cout << bldg_ctr;
