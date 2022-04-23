d6ca34bfba09b0fbaf317b92aec7667e3ab576fc
dongwook-chan <dongwook.chan@gmail.com>
2022-04-23 15:28:20 +0900

    solved problem 1080

---
+++ BOJ/1080/1080.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <cstring>
+
+#define MAX_ARRAY_SIZE 50
+#define OOM(y, x) (!(0 <= (y) && (y) < N && 0 <= (x) && (x) < M))
+
+using namespace std;
+
+int N, M;
+string before[MAX_ARRAY_SIZE];
+string after[MAX_ARRAY_SIZE];
+
+int ctr;
+
+void flip(int y, int x) {
+    for(int dy = 0; dy < 3; ++dy) {
+        for(int dx = 0; dx < 3; ++dx) {
+            before[y + dy][x + dx] = 1 - (before[y + dy][x + dx] - '0') + '0';
+        }
+    }
+    ++ctr;
+}
+
+int main(){
+    cin >> N >> M;
+
+    for(int y = 0; y < N; ++y) {
+        cin >> before[y];
+    }
+
+    for(int y = 0; y < N; ++y) {
+        cin >> after[y];
+    }
+
+    for(int y = 0; y < N - 2; ++y) {
+        for(int x = 0; x < M - 2; ++x) {
+            if(before[y][x] != after[y][x]) {
+                flip(y, x);
+            }
+        }
+    }
+
+    if(memcmp(after, before, sizeof(before))) {
+        cout << -1;
+    }
+    else{
+        cout << ctr;
+    }
+
+    return 0;
+}
