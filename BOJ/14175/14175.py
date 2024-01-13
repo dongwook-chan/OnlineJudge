34446e3c27fba9e694c09b94e2ac5208cb85e6cd
dongwook-chan <dongwook.chan@gmail.com>
2024-01-13 13:32:42 +0900

    solved problem 14175

--- BOJ/14175/14175.py
+++ BOJ/14175/14175.py
@@ -2,6 +2,8 @@ M, N, K = map(int, input().split())
 for _ in range (M):
     line = ""
     for c in input():
+        if c.isspace():
+            continue
         line += c * K
     line += "\n"
     print(line * K)
