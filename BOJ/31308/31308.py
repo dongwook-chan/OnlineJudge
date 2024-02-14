098bc039ee0307b48c7dd3e2d83c716c295fb395
dongwook-chan <dongwook.chan@gmail.com>
2024-02-14 22:26:03 +0900

    solved problem 31308

--- BOJ/31308/31308.py
+++ BOJ/31308/31308.py
@@ -1,7 +1,29 @@
 n, k, p = map(int, input().split())
+
+
+def getMyDivisor(n):
+    divisorsList = []
+
+    for i in range(1, int(n ** (1 / 2)) + 1):
+        if (n % i == 0):
+            divisorsList.append(i)
+            if ((i ** 2) != n):
+                divisorsList.append(n // i)
+
+    divisorsList.sort()
+
+    return divisorsList
+
+l1 = getMyDivisor(n)
+l2 = l1[::-1]
+
 ans = []
-for i in range(p, 0, -1):
-    if n % i == 0 and n // i <= k:
-        ans.append(n // i)
+for e1, e2 in zip(l1, l2):
+    if e2 > p:
+        continue
+    if e1 > k:
+        break
+    ans.append(e1)
+
 print(len(ans))
 print('\n'.join(map(str, ans)))
