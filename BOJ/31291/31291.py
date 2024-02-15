491f04c3ae6c15234657f2a74f62c7a456b8acc7
dongwook-chan <dongwook.chan@gmail.com>
2024-02-15 10:28:14 +0900

    solved problem 31291

--- BOJ/31291/31291.py
+++ BOJ/31291/31291.py
@@ -2,26 +2,26 @@ right_item_with_colors = ["white Barabashka", "blue book", "red chair", "gray mo
 
 for _ in range(5):
     sentence = input()
-    sentence = sentence.replace("'", "").replace(",", "").replace("-", "").replace(".", "")
+    sentence = sentence.replace("'", " ").replace(",", " ").replace("-", " ").replace(".", " ")
     # sentence_lower = sentence.lower()
 
-    visited_colors = {"white": False, "blue": False, "red": False, "gray": False, "green": False}
-    visited_items = {"Barabashka": False, "book": False, "chair": False, "mouse": False, "bottle": False}
-    for right_item_with_color in right_item_with_colors:
-        if right_item_with_color in sentence:
-            print(right_item_with_color)
+    visited_colors = {"white": None, "blue": None, "red": None, "gray": None, "green": None}
+    visited_items = {"barabashka": None, "book": None, "chair": None, "mouse": None, "bottle": None}
+
+    for word_original in sentence.split():
+        word = word_original.lower()
+        if word in visited_colors:
+            visited_colors[word] = word_original
+        elif word in visited_items:
+            visited_items[word] = word_original
+
+    for color, item in zip(visited_colors.values(), visited_items.values()):
+        if color is not None and item is not None:
+            print(color, item)
             break
     else:
-        for word in sentence.split():
-            if word != "Barabashka":
-                word = word.lower()
-            if word in visited_colors:
-                visited_colors[word] = True
-            elif word in visited_items:
-                visited_items[word] = True
-
         for color, item in zip(visited_colors.keys(), visited_items.keys()):
-            if visited_colors[color] or visited_items[item]:
+            if visited_colors[color] is not None or visited_items[item] is not None:
                 continue
             print(color, item)
             break
