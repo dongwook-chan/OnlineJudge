c0250c2e6d2f1ae9c4b62ecb3c0a2c1f80b6aeb8
dongwook-chan <dongwook.chan@gmail.com>
2024-02-25 13:31:34 +0900

    solved problem 28310

--- BOJ/28310/28310.py
+++ BOJ/28310/28310.py
@@ -1,12 +1,11 @@
 from fractions import Fraction
 for _ in range(int(input())):
     N, M = map(int, input().split())
-    V, A = [], []
+    A = []
     for j in range(M):
         v, *Aj = map(int, input().split())
-        V.append(v)
         A.append([Fraction(Aji, v) for Aji in Aj])
-
-    A_sum = sorted([sum([A[j][i] for j in range(M)]) for i in range(N)])
-
-    print(A_sum[-1] - A_sum[0])
+    A_sum = [sum([A[j][i] for j in range(M)]) for i in range(N)]
+    A_max = max(A_sum)
+    A_min = min(A_sum)
+    print(A_max - A_min)
