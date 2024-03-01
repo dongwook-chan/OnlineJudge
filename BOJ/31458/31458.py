6bc6b7db0179e8aac500cf047cc101941756dea1
dongwook-chan <dongwook.chan@gmail.com>
2024-03-01 23:36:44 +0900

    solved problem 31458

--- BOJ/31458/31458.py
+++ BOJ/31458/31458.py
@@ -3,6 +3,6 @@ for _ in range(int(input())):
     e = input()
     i = e.find('0') + e.find('1') + 1
     n = 1 if len(e) - i >= 2 else int(e[i])
-    if i % 2:
+    if i & 1:
         n = 1 - n
     print(n)
