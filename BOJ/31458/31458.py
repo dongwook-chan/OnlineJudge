e5dbd2cbfda0ee5ee0d6ec04de110d6bf139ef51
dongwook-chan <dongwook.chan@gmail.com>
2024-03-01 23:35:23 +0900

    solved problem 31458

--- BOJ/31458/31458.py
+++ BOJ/31458/31458.py
@@ -1,17 +1,8 @@
 from collections import deque
 for _ in range(int(input())):
-    expression = deque(input())
-    logical_not = False
-    while expression[0] == '!':
-        logical_not = not logical_not
-        expression.popleft()
-
-    number = int(expression.popleft())
-
-    if expression and expression[0] == '!':
-        number = 1
-
-    if logical_not:
-        number = 1 - number
-
-    print(number)
+    e = input()
+    i = e.find('0') + e.find('1') + 1
+    n = 1 if len(e) - i >= 2 else int(e[i])
+    if i % 2:
+        n = 1 - n
+    print(n)
