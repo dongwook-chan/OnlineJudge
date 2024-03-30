83a55abf06b7291b6d3a534e0164c967c656a4b5
dongwook-chan <dongwook.chan@gmail.com>
2024-03-30 17:28:08 +0900

    solved problem 31678

---
+++ BOJ/31678/31678.py
@@ -0,0 +1,22 @@
+names = []
+bingo_candidates_list = []
+for n in range(int(input())):
+    names.append(input())
+
+    board = tuple(tuple(map(int, input().split())) for _ in range(5))
+    bingo_candidates = []
+    bingo_candidates += [set(board[i]) for i in range(5)]
+    bingo_candidates += [set(board[i][j] for i in range(5)) for j in range(5)]
+    bingo_candidates.append(set(board[i][i] for i in range(5)))
+    bingo_candidates.append(set(board[4 - i][i] for i in range(5)))
+    bingo_candidates_list.append(bingo_candidates)
+
+input()
+bingo_names = []
+selected_balls = set(map(int, input().split()))
+for name, bingo_candidates in zip(names, bingo_candidates_list):
+    for bingo_candidate in bingo_candidates:
+        if bingo_candidate <= selected_balls:
+            bingo_names.append(name)
+print(len(bingo_names))
+print('\n'.join(bingo_names))
