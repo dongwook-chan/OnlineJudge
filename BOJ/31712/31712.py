0929b4ce28b5dd2459704880ca10dbb50b35cc6e
dongwook-chan <dongwook.chan@gmail.com>
2024-04-02 17:20:44 +0900

    solved problem 31712

---
+++ BOJ/31712/31712.py
@@ -0,0 +1,22 @@
+from queue import PriorityQueue
+from collections import namedtuple
+
+Event = namedtuple("Event", ["time", "cycle", "damage"])
+
+Cu, Du = map(int, input().split())
+Cd, Dd = map(int, input().split())
+Cp, Dp = map(int, input().split())
+H = int(input())
+
+pq: PriorityQueue[Event] = PriorityQueue()
+pq.put(Event(0, Cu, Du))
+pq.put(Event(0, Cd, Dd))
+pq.put(Event(0, Cp, Dp))
+while H > 0:
+    Tnow = pq.queue[0].time
+    while pq.queue[0].time == Tnow:
+        T, C, D = pq.get()
+        H -= D
+        pq.put(Event(T + C, C, D))
+
+print(Tnow)
