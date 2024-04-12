32e3ed3f93c02f6e702483e682206d2cd40ea530
dongwook-chan <dongwook.chan@gmail.com>
2024-04-12 22:49:16 +0900

    solved problem 31607

--- BOJ/31607/31607.py
+++ BOJ/31607/31607.py
@@ -1,2 +1,7 @@
-l=list(map(int,open(0)))
-print(int(any(sum(l)==2*x for x in l)))
+# A,B,C=map(int,open(0))
+# print(int(A+B==C or A+C==B or B+C==A))
+
+a,b,c=sorted(map(int,open(0)))
+print(int(a+b==c))
+
+# print(any(map(lambda x: sum(l)==2*x,l:=list(map(int,open(0))))))
