e2b8bbbedc0d84fccb298e68de23345afa63169d
dongwook-chan <dongwook.chan@gmail.com>
2024-04-12 22:54:21 +0900

    solved problem 31607

--- BOJ/31607/31607.py
+++ BOJ/31607/31607.py
@@ -1,7 +1,10 @@
 # A,B,C=map(int,open(0))
 # print(int(A+B==C or A+C==B or B+C==A))
 
-a,b,c=sorted(map(int,open(0)))
-print(int(a+b==c))
+l=list(map(int,open(0)))
+print(int(sum(l)==max(l)*2))
+
+# a,b,c=sorted(map(int,open(0)))
+# print(int(a+b==c))
 
 # print(any(map(lambda x: sum(l)==2*x,l:=list(map(int,open(0))))))
