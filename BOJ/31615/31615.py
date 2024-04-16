fbdb6d1c1baf405e6e8f96165cf1c83c3308196a
dongwook-chan <dongwook.chan@gmail.com>
2024-04-17 00:22:28 +0900

    solved problem 31615

--- BOJ/31615/31615.py
+++ BOJ/31615/31615.py
@@ -1,3 +1,3 @@
 # print(len(str(sum(map(int,open(0))))))
-print(len(f"{sum(map(int,open(0)))}"))
-# print(len('%s'%sum(map(int,open(0)))))
+# print(len(f"{sum(map(int,open(0)))}"))
+print(len('%s'%sum(map(int,open(0)))))
