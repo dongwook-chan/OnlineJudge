1e1385526e6a0c5a73ca94a607f1bcc316ff4bad
dongwook-chan <dongwook.chan@gmail.com>
2024-04-18 00:00:02 +0900

    solved problem 31738

--- BOJ/31738/31738.py
+++ BOJ/31738/31738.py
@@ -1,4 +1,8 @@
 from functools import reduce
 import operator
 N, M = map(int, input().split())
-print(reduce(operator.mul, [i % M for i in range(1, N + 1)]) % M)
+a = 1
+for i in range(2, N+1):
+    a = a * (i % M)
+    a %= M
+print(a)
