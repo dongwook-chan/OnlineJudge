0054f016c0ce1ccf884af41768e2de5dedf3534d
dongwook-chan <dongwook.chan@gmail.com>
2024-07-02 19:07:27 +0900

    solved problem 31907

---
+++ BOJ/31907/31907.py
@@ -0,0 +1,8 @@
+K = int(input())
+P = []
+for _ in range(K):
+    print("G" * K + "." * 3 * K)
+for _ in range(K):
+    print("." * K + "I" * K + "." * K + "T" * K)
+for _ in range(K):
+    print("." * 2 * K + "S" * K + "." * K)
