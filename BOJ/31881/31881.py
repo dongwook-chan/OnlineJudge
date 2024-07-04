b48b5862e3f7dc35e395d46c9704ece0f52cab68
dongwook-chan <dongwook.chan@gmail.com>
2024-07-05 00:29:28 +0900

    solved problem 31881

--- BOJ/31881/31881.py
+++ BOJ/31881/31881.py
@@ -1,12 +1,12 @@
 import sys
 from collections import Counter
-N, Q = map(int, sys.stdin.readline().split())
+N, Q = map(int, input().split())
 c = [None] + [False] * N
 for _ in range(Q):
-    q, *x_ = sys.stdin.readline().split()
-    match q:
+    q = sys.stdin.readline()
+    match q[0]:
         case "1" | "2":
-            x = int(x_[0])
-            c[x] = q == "1"
+            x = int(q[2:])
+            c[x] = q[0] == "1"
         case "3":
             print(Counter(c)[False])
