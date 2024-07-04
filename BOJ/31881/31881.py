3b9ca84600a77c5d6bc407bbb8bdbd2aa4902c7c
dongwook-chan <dongwook.chan@gmail.com>
2024-07-05 00:36:39 +0900

    solved problem 31881

--- BOJ/31881/31881.py
+++ BOJ/31881/31881.py
@@ -2,11 +2,21 @@ import sys
 from collections import Counter
 N, Q = map(int, input().split())
 c = [None] + [False] * N
+ctr = 10
 for _ in range(Q):
     q = sys.stdin.readline()
     match q[0]:
-        case "1" | "2":
+        case "1":
             x = int(q[2:])
-            c[x] = q[0] == "1"
+            if q[0] == "1":
+                if c[x] == False:
+                    ctr -= 1
+                c[x] = True
+        case "2":
+            x = int(q[2:])
+            if q[0] == "2":
+                if c[x] == True:
+                    ctr += 1
+                c[x] = False
         case "3":
-            print(Counter(c)[False])
+            print(ctr)
