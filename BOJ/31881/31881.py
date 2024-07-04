8d7d156432576d2fb8e54bfb3f545e72f233719e
dongwook-chan <dongwook.chan@gmail.com>
2024-07-05 00:24:53 +0900

    solved problem 31881

--- BOJ/31881/31881.py
+++ BOJ/31881/31881.py
@@ -1,9 +1,9 @@
 import sys
 from collections import Counter
-N, Q = map(int, sys.stdin.readline().rstrip().split())
+N, Q = map(int, sys.stdin.readline().split())
 c = [None] + [False] * N
-for _ in range(N):
-    q, *x_ = sys.stdin.readline().rstrip().split()
+for _ in range(Q):
+    q, *x_ = sys.stdin.readline().split()
     match q:
         case "1" | "2":
             x = int(x_[0])
